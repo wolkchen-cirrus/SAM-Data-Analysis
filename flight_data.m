1c9b228dc37cbea34d7bab20c8fd320716bc3903
JGirdwood <42206925+JGirdwood@users.noreply.github.com>
2018-12-06 22:28:50 +0000

    fog data

--- flight_data.m
+++ flight_data.m
@@ -1,6 +1,9 @@
 close, clear, clc
 format long
 
+outfile = false;
+plotdata = true;
+
 date = '18-11-02';
 log_file = 'DATA01.CSV';
 pix_file = '2018-11-02 16-00-07.bin-61468.mat';
@@ -31,30 +34,33 @@ altitudes = [10,50,70,100,120];
 slicestruct = sliceData(profiles,altitudes,pindex,counts);
 intParams = colint(profiles);
 
-% genfile('SAM_DATA',log_file,pix_file,date,profiles);
+if outfile == true
+    genfile('SAM_DATA',log_file,pix_file,date,profiles);
+end
 
-fig = figure(1);
-ax1 = subplot(1,2,1);
-tln1 = plot([profiles.profile_1.temperature],[profiles.profile_1.altitude],'x');
-hold on
-tln2 = plot([profiles.profile_2.temperature],[profiles.profile_2.altitude],'x');
-hold on
-ylim([10,140])
-xlabel(sprintf('Temperature%cC',char(176)));
-ylabel('Altitude (m)');
-title(['Temperature Profile ',date])
-legend(ax1,{'Ascent','Descent'},'Location','northeast')
-
-ax2 = subplot(1,2,2);
-mln1 = plot([profiles.profile_1.particle_mass_conc],[profiles.profile_1.altitude]);
-hold on
-mln2 = plot([profiles.profile_2.particle_mass_conc],[profiles.profile_2.altitude]);
-hold on
-ylim([10,140])
-xlabel('Particle Mass Concentration (kgm^-^3)');
-ylabel('Altitude (m)');
-title(['Particle Mass Concentration Profile ',date])
-legend(ax2,{'Ascent','Descent'},'Location','northeast')
-hold off
-
+if plotdata == true
+    fig = figure(1);
+    ax1 = subplot(1,2,1);
+    tln1 = plot([profiles.profile_1.temperature],[profiles.profile_1.altitude],'x');
+    hold on
+    tln2 = plot([profiles.profile_2.temperature],[profiles.profile_2.altitude],'x');
+    hold on
+    ylim([10,140])
+    xlabel(sprintf('Temperature%cC',char(176)));
+    ylabel('Altitude (m)');
+    title(['Temperature Profile ',date])
+    legend(ax1,{'Ascent','Descent'},'Location','northeast')
+    
+    ax2 = subplot(1,2,2);
+    mln1 = plot([profiles.profile_1.particle_mass_conc],[profiles.profile_1.altitude]);
+    hold on
+    mln2 = plot([profiles.profile_2.particle_mass_conc],[profiles.profile_2.altitude]);
+    hold on
+    ylim([10,140])
+    xlabel('Particle Mass Concentration (kgm^-^3)');
+    ylabel('Altitude (m)');
+    title(['Particle Mass Concentration Profile ',date])
+    legend(ax2,{'Ascent','Descent'},'Location','northeast')
+    hold off
+end
 
